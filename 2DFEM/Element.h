294f07f13c92be7f76859c33682d8524380e15a8
Haohang Huang <hhuang81@illinois.edu>
2018-01-29 01:18:51 -0600

    file

---
+++ 2DFEM/Element.h
@@ -0,0 +1,15 @@
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+class Element {
+  public:
+    Element();
+
+    double ux;
+    double uy;
+
+  private:
+
+};
+
+#endif
---
+++ 2DFEM/Node.h
@@ -0,0 +1,15 @@
+#ifndef NODE_H
+#define NODE_H
+
+class Node {
+  public:
+    Node();
+
+    double ux;
+    double uy;
+
+  private:
+
+};
+
+#endif
