f66d24aec7691a80b19a5824806bb2eb59329ad5
Haohang Huang <hhuang81@illinois.edu>
2018-01-30 17:45:01 -0600

    file

--- 2DFEM/test.cpp
+++ 2DFEM/test.cpp
@@ -1,17 +1,8 @@
-#include <iostream>
+#include "Eigen/Eigen"
 
+using namespace Eigen;
 using namespace std;
 
-void stir(int & x, int * y) {
-        x = 7;
-        *y = 10;
-        //y = new int(10);
-}
-
 int main() {
-        int w;     int * v;
-        v = 10;
-        stir(w, v);
-        cout << w << ", " << *v << endl;
-        delete v;   return 0;
+  int a = 1;
 }
