151216db7e6f998810ab951eab3fa6931c44a96d
Haohang Huang <hhuang81@illinois.edu>
2018-02-10 12:19:58 -0600

    file

--- FEM/FEM/test.cpp
+++ FEM/FEM/test.cpp
@@ -2,20 +2,21 @@
 #include <fstream>
 #include <string>
 
+using namespace std;
+
 int main() {
-    std::ifstream inFile("meshData.txt");
-
-    if(!inFile) {
-        std::cout << "Cannot open input file." << std::endl;
-        return 1;
-    }
-
-    std::string byLine;
-    while(inFile) {
-        std::getline(inFile, byLine);
-        std::cout << byLine << std::endl;
-    }
-    std::cout << "hello" << std::endl;
-    inFile.close();
+    /*
+    int* arr[3];
+    int a=0, b=0, c=0;
+    arr[0] = &a;
+    arr[1] = &b;
+    arr[2] = &c;
+    cout<< arr[2] <<endl;
+    */
+    int** List = new int*[3];
+    List[0] = new int(0);
+    int* b = new int(1); List[1] = b;
+    int* c = new int(2); List[2] = c;
+    cout<< *List[2] <<endl;
     return 0;
 }
